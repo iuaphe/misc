4a11058d107e3b07a6419c24897d8f089dfeb075
acou12 <51276518+acou12@users.noreply.github.com>
2023-06-09 19:10:02 -0400

    cells

--- src/frag.glsl
+++ src/frag.glsl
@@ -8,21 +8,36 @@ in vec3 v_color;
 
 out vec4 outColor;
 
+vec2 grad( ivec2 z ) {
+    int n = z.x+z.y*11111;
+    n = (n<<13)^n;
+    n = (n*(n*n*15731+789221)+1376312589)>>16;
+    // simple random vectors
+    return vec2(cos(float(n)),sin(float(n)));
+}
+
+float noise(in vec2 p) {
+    ivec2 i = ivec2(floor(p));
+     vec2 f = fract(p);
+	
+	vec2 u = f*f*(3.0-2.0*f); // feel free to replace by a quintic smoothstep instead
+
+    return mix( mix( dot( grad( i+ivec2(0,0) ), f-vec2(0.0,0.0) ), 
+                     dot( grad( i+ivec2(1,0) ), f-vec2(1.0,0.0) ), u.x),
+                mix( dot( grad( i+ivec2(0,1) ), f-vec2(0.0,1.0) ), 
+                     dot( grad( i+ivec2(1,1) ), f-vec2(1.0,1.0) ), u.x), u.y);
+}
+
 void main() {
-    float slices = 20.0;
-    float angleSlices = 30.0;
-    float dist = distance(vec2(0.0, 0.0), v_position);
-    float dispDist = dist - v_time / 5.0;
-    float angle = atan(v_position.y, v_position.x);
-    float distSnapped = floor(dispDist * slices) / slices;
-    float angleSnapped = floor((angle + (dist - 0.5) * v_time) / 6.28318 * angleSlices) * 6.28318 / angleSlices;
-    // float distSnapped = dist;
-    vec3 color = (sin(vec3(
-        distSnapped * 31.4 / 1.6 + v_time + angleSnapped * 3.0 * (mod(distSnapped * slices, 3.0) + 1.0),
-        distSnapped * 31.4 / 1.2 + v_time,
-        distSnapped * 31.4 / 2.1 + v_time
-    )) + 1.0) / 2.0;
-    outColor = vec4(
+   vec3 color = vec3(0.0, 0.0, 0.0);
+
+   vec2 uv = (v_position + 1.0) / 2.0;
+
+   uv *= 20.0;
+
+   color = floor(vec3(noise(uv + v_time * 5.0) * 2.0) * 5.0) / 5.0;
+
+   outColor = vec4(
         color,
     1.0);
 }
