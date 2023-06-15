7033e888634369a40dca8b97d39e65d1a22cdf23
acou12 <51276518+acou12@users.noreply.github.com>
2023-06-15 18:39:52 -0400

    uhhh

--- src/frag.glsl
+++ src/frag.glsl
@@ -2,17 +2,49 @@
 
 precision highp float;
 
-uniform vec2 u_center;
-uniform vec3 u_color;
+uniform sampler2D u_image;
 
 in float v_time;
 in vec2 v_position;
-in vec3 v_color;
 
 out vec4 outColor;
 
 void main() {
-   outColor = vec4(
-        u_color,
-    1.0) * (1.0 - step(0.05, distance(u_center, v_position)));
+    vec2 coords = (v_position + 1.0) / 2.0;
+    coords.y = 1.0 - coords.y;
+
+    // WAVY
+    coords.x += sin(coords.y * 10.0 + v_time / 10.0) / 30.0;
+    coords.y += sin(coords.x * 10.0 + v_time / 10.0) / 30.0;
+    
+    // DICE
+    // coords.y += sin(v_time / 100.0) * 120.0 / 100.0 * (mod(floor(coords.x * 20.0) * 10.0, 7.0) / 20.0 * 5.0 + 1.0);
+    // coords.y = mod(coords.y, 1.0);
+
+    //PIXELIZE
+    // float res = v_time;
+    // coords = floor(coords * res) / res;
+
+    // TWIST
+    // vec2 centerVec = coords - vec2(0.5);
+    // float angle = atan(centerVec.y, centerVec.x);
+    // float len = length(centerVec);
+
+    // angle += len * (((pow(v_time, 1.5) / 10.0) / 50.0)) * 3.0;
+
+    // coords = len * vec2(cos(angle), sin(angle)) + vec2(0.5);
+
+    outColor = vec4(texture(u_image, coords));
+
+    // outColor = vec4(
+    //       pow(
+    //         length(texture(u_image, coords)
+    //         - texture(u_image, coords + vec2(0.005, 0.00))), 
+    //         2.0),
+    //       pow(
+    //         length(texture(u_image, coords)
+    //         - texture(u_image, coords + vec2(0.00, 0.005))), 
+    //         2.0),
+    //       0.0
+    // , 1.0);
 }
--- src/main.ts
+++ src/main.ts
@@ -21,98 +21,98 @@ const createVao = (
 
 const canvas = document.querySelector("canvas")!;
 
-const gl = canvas.getContext("webgl2")!;
+const image = new Image();
+image.src = "/mochi.png";
 
-const vertShader = gl.createShader(gl.VERTEX_SHADER)!;
-gl.shaderSource(vertShader, vert);
-gl.compileShader(vertShader);
-if (!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS)) {
-  throw new Error(gl.getShaderInfoLog(vertShader)!);
-}
-
-const fragShader = gl.createShader(gl.FRAGMENT_SHADER)!;
-gl.shaderSource(fragShader, frag);
-gl.compileShader(fragShader);
-if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
-  throw new Error(gl.getShaderInfoLog(fragShader)!);
-}
-
-const program = gl.createProgram()!;
-gl.attachShader(program, vertShader);
-gl.attachShader(program, fragShader);
-gl.linkProgram(program);
-
-gl.useProgram(program);
-
-const positionLocation = gl.getAttribLocation(program, "a_position")!;
-
-const timeUniformLocation = gl.getUniformLocation(program, "u_time")!;
-const centerUniformLocation = gl.getUniformLocation(program, "u_center")!;
-const colorUniformLocation = gl.getUniformLocation(program, "u_color")!;
-const ratioUniformLocation = gl.getUniformLocation(program, "u_ratio")!;
-
-type Square = {
-  vao: WebGLVertexArrayObject;
-  speed: number;
-  color: [number, number, number];
-  center: [number, number];
+image.onload = () => {
+  render(image);
 };
 
-const squares: Square[] = [];
+const render = (image: TexImageSource) => {
+  const gl = canvas.getContext("webgl2")!;
 
-const squareCount = 20;
-
-for (let intY = 0; intY < squareCount; intY++) {
-  let x = 0;
-  let y = intY / (squareCount / 2) - 1;
-  const squareSize = 2 / squareCount;
-  squares.push({
-    vao: createVao(gl, positionLocation, [
-      x,
-      y,
-      x + squareSize,
-      y,
-      x + squareSize,
-      y + squareSize,
-      x,
-      y,
-      x,
-      y + squareSize,
-      x + squareSize,
-      y + squareSize,
-    ]),
-    speed: Math.random() + 0.5,
-    center: [x + squareSize / 2, y + squareSize / 2],
-    color: [Math.random(), Math.random(), Math.random()],
-  });
-}
-
-gl.useProgram(program);
-
-const primitiveType = gl.TRIANGLES;
-const count = 6;
-
-let u = 0;
-
-const draw = () => {
-  requestAnimationFrame(draw);
-
-  canvas.width = window.innerWidth * 1.5;
-  canvas.height = window.innerHeight * 1.5;
-
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-  u += 0.01;
-
-  gl.uniform1f(ratioUniformLocation, canvas.width / canvas.height);
-
-  for (const square of squares) {
-    gl.bindVertexArray(square.vao);
-    gl.uniform1f(timeUniformLocation, u * square.speed);
-    gl.uniform2f(centerUniformLocation, ...square.center);
-    gl.uniform3f(colorUniformLocation, ...square.color);
-    gl.drawArrays(primitiveType, 0, count);
+  const vertShader = gl.createShader(gl.VERTEX_SHADER)!;
+  gl.shaderSource(vertShader, vert);
+  gl.compileShader(vertShader);
+  if (!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS)) {
+    throw new Error(gl.getShaderInfoLog(vertShader)!);
   }
-};
 
-draw();
+  const fragShader = gl.createShader(gl.FRAGMENT_SHADER)!;
+  gl.shaderSource(fragShader, frag);
+  gl.compileShader(fragShader);
+  if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS)) {
+    throw new Error(gl.getShaderInfoLog(fragShader)!);
+  }
+
+  const program = gl.createProgram()!;
+  gl.attachShader(program, vertShader);
+  gl.attachShader(program, fragShader);
+  gl.linkProgram(program);
+
+  gl.useProgram(program);
+
+  const positionLocation = gl.getAttribLocation(program, "a_position")!;
+
+  const timeUniformLocation = gl.getUniformLocation(program, "u_time")!;
+  const ratioUniformLocation = gl.getUniformLocation(program, "u_ratio")!;
+  const imageUniformLocation = gl.getUniformLocation(program, "u_image")!;
+
+  const vao = createVao(
+    gl,
+    positionLocation,
+    [-1, -1, 1, -1, 1, 1, -1, -1, -1, 1, 1, 1]
+  );
+
+  gl.useProgram(program);
+
+  const texture = gl.createTexture()!;
+
+  gl.activeTexture(gl.TEXTURE0);
+
+  gl.bindTexture(gl.TEXTURE_2D, texture);
+
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+  // Upload the image into the texture.
+  var mipLevel = 0; // the largest mip
+  var internalFormat = gl.RGBA; // format we want in the texture
+  var srcFormat = gl.RGBA; // format of data we are supplying
+  var srcType = gl.UNSIGNED_BYTE; // type of data we are supplying
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    mipLevel,
+    internalFormat,
+    srcFormat,
+    srcType,
+    image
+  );
+
+  const primitiveType = gl.TRIANGLES;
+  const count = 6;
+
+  let u = 0;
+
+  const draw = () => {
+    requestAnimationFrame(draw);
+
+    canvas.width = window.innerWidth * 1.5;
+    canvas.height = window.innerHeight * 1.5;
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+    u += 1;
+
+    gl.uniform1f(ratioUniformLocation, canvas.width / canvas.height);
+    gl.uniform1f(timeUniformLocation, u);
+    gl.uniform1i(imageUniformLocation, gl.TEXTURE0);
+
+    gl.bindVertexArray(vao);
+    gl.drawArrays(primitiveType, 0, count);
+  };
+
+  draw();
+};
