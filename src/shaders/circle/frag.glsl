da7d7195d3cb1a471c77c3c23c698381c9d36f90
acou12 <51276518+acou12@users.noreply.github.com>
2024-04-14 15:57:20 -0400

    alternative distances

--- src/shaders/circle/frag.glsl
+++ src/shaders/circle/frag.glsl
@@ -11,6 +11,9 @@ out vec4 outColor;
  
 void main() {
   float dist = sqrt(v_position.x * v_position.x + v_position.y * v_position.y);
+  // float dist = abs(v_position.x) + abs(v_position.y);
+  // float dist = abs(sin(v_position.x * 2.0)) + abs(sin(v_position.y * 2.0));
+
   // outColor = vec4(1.0 - step(0.9, dist)) * vec4(u_color, 1.0);
   outColor = 
                       (1.0 - step(0.8, dist)) * vec4(u_color, 1.0) +
